33971e04a511f83d07d2cfe07d3a144d553fd661
Nishant Jain <nsj0002@gmail.com>
2025-02-07 22:33:07 -0500

    more code

--- Tests/ActualWorkTests.cs
+++ Tests/ActualWorkTests.cs
@@ -1,3 +1,4 @@
+using System.IO.Abstractions;
 using DemoConsole;
 using NSubstitute;
 
@@ -5,11 +6,11 @@ namespace Tests;
 
 public class Act
 {
-    private readonly Logger loggerMock = Substitute.For<Logger>();
+    private readonly Logger loggerMock = Substitute.For<Logger>(new FileSystem());
     [Fact]
     public async Task Test_LogCalledSameNumberOfTimesAsIteration()
     {
-        ActualWork work = new(new Logger());
+        ActualWork work = new(loggerMock);
 
         await work.Process(4, 1000);
         await loggerMock.Received(4).Log(Arg.Any<string>());
